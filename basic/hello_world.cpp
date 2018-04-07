7887b22e44bb5fd1e484229ef45142fb9438ed9e
arifikhsan <ryugahideki9999@gmail.com>
2018-04-07 19:24:27 +0700

    update

--- basic/hello_world.cpp
+++ basic/hello_world.cpp
@@ -1,8 +1,35 @@
 #include <iostream>
+#include <math.h>
 
 using namespace std;
 
 int main() {
-    cout << "Hello World" << endl;
-    return 0;
+    int besar_kotak;
+
+    cout << "Besarnya berapa: "; cin >> besar_kotak;
+
+    for (int i = 1; i <= besar_kotak; i++)
+    {
+        cout << "* ";
+    }
+
+    for (int k = 1; k <= besar_kotak - 2; k++)
+    {
+        cout << "* ";
+        for (int l = 1; l <= (besar_kotak - 2) * 2; l++)
+        {
+            cout << " ";
+        }
+        cout << "*";
+        cout << endl;
+    }
+
+    for (int j = 1; j <= besar_kotak; j++)
+    {
+        cout << "* ";
+    }
+
+    cout << endl;
+    
+
 }
